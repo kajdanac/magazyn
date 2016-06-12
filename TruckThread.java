92e78eb6f1c998bf65cc65e37e598899537d9feb
kajdanac <kajdanac@gmail.com>
2016-06-12 15:37:45 +0200

    Create TruckThread.java

---
+++ TruckThread.java
@@ -0,0 +1,16 @@
+
+public class TruckThread implements Runnable
+{
+	Truck t;
+	TruckThread(Truck t)
+	{
+		this.t = t;
+	}
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
