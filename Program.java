95cb001eee1cc1d48ece454b4e1ade63757fd259
kajdanac <kajdanac@gmail.com>
2016-06-12 15:30:55 +0200

    Update Program.java

--- Program.java
+++ Program.java
@@ -7,6 +7,20 @@ public class Program
 	}
 	public void run()
 	{
+		init();
+		EventQueue.invokeLater(new Runnable()
+		{
+			
+			@Override
+			public void run()
+			{
+				window = new Window();
+				window.setVisible(true);
+			}
+		});
+	}
+	public void init()
+	{
 		
 	}
 }
